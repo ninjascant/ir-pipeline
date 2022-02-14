9d9002f1a3c1ca943234d91d1d8781cd8bb77088
Maxim Razhev <cvlt.mao@gmail.com>
2022-02-14 22:40:19 +0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name="ir-pipeline",
+    version='0.0.1',
     package_dir={"": "src"},
-    packages=find_packages('src'),
+    packages=find_packages("src"),
     install_requires=[
-        "click ==^8.0.3",
-        "torch==^1.10.1",
-        "transformers==^4.15.0"
-        "datasets==^1.17.0"
-        "faiss-cpu==^1.7.2"
-        "tqdm==^4.62.3"
-        "loguru==^0.5.3"
+        "click==8.0.3",
+        "torch==1.10.0",
+        "transformers==4.15.0",
+        "datasets==1.17.0",
+        "faiss-cpu==1.7.2",
+        "tqdm==4.62.3",
+        "loguru==0.5.3",
     ],
     entry_points={
         'console_scripts': [
-            'irpipeline=irpipeline.cli:run',
+            'irpipeline=ir_pipeline.cli:run',
         ],
     }
 )
